a8cd68510b7dbd9741196164118938cf4994f16b
Tom433 <tom@wolske.tv>
2016-05-25 10:18:11 +0200

    Add Lichtsteuerung.asm

---
+++ dev/Lichtsteuerung.asm
@@ -0,0 +1,20 @@
+;----------------------------------
+; Nappy's Home
+;----------------------------------
+CSEG AT 0H
+LJMP Anfang
+ORG 100H
+
+EIN1 EQU 20H
+START1 EQU EIN1.0
+START2 EQU EIN1.1
+STOP1 EQU EIN1.2
+STOP2 EQU EIN1.3
+G_AUF EQU EIN1.5
+G_ZU EQU EIN1.6
+G_KONT EQU EIN1.7
+
+licht_AN:
+	
+licht_AUS:
+	
